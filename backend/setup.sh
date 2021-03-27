b69a95ab904e455c59daa7665d78c424eb89f0ed
Alan Scott <alan@Alans-MacBook-Pro.local>
2021-03-27 11:19:43 -0700

    removing db URL from setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL="postgresql://uwmmdlhsbkftvo:4e3fa44f2aafcf456eef985f7a42cfabec134e192ae6878a4d1d5b5f86a0ae28@ec2-54-225-190-241.compute-1.amazonaws.com:5432/d7fata0a04nr6m"
+# export DATABASE_URL="postgresql://uwmmdlhsbkftvo:4e3fa44f2aafcf456eef985f7a42cfabec134e192ae6878a4d1d5b5f86a0ae28@ec2-54-225-190-241.compute-1.amazonaws.com:5432/d7fata0a04nr6m"
 export AUTH0_DOMAIN = 'alanscott.us.auth0.com'
 export ALGORITHMS = ['RS256']
 export API_AUDIENCE = 'casting'

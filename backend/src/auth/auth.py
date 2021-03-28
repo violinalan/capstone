25c59301ed2e0679e66b56722b122d8826a914e3
Alan Scott <alan@Alans-MacBook-Pro.local>
2021-03-27 17:44:09 -0700

    updated auth0 config to grab environment variables

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,3 +1,4 @@
+import os
 import json
 from flask import request, _request_ctx_stack
 from functools import wraps
@@ -8,6 +9,10 @@ from urllib.request import urlopen
 # AUTH0_DOMAIN = 'alanscott.us.auth0.com'
 # ALGORITHMS = ['RS256']
 # API_AUDIENCE = 'casting'
+
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 ## AuthError Exception
 '''
